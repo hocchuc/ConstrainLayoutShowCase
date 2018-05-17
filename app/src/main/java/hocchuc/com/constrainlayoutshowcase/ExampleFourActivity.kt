7fe0e8faa10965f22daea39daab925e8c39c6349
howard <howard@vinova.sg>
2018-05-17 14:30:22 +0700

    version 1

---
+++ app/src/main/java/hocchuc/com/constrainlayoutshowcase/ExampleFourActivity.kt
@@ -0,0 +1,12 @@
+package hocchuc.com.constrainlayoutshowcase
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ExampleFourActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_four)
+    }
+}
--- app/src/main/java/hocchuc/com/constrainlayoutshowcase/MainActivity.kt
+++ app/src/main/java/hocchuc/com/constrainlayoutshowcase/MainActivity.kt
@@ -2,11 +2,29 @@ package hocchuc.com.constrainlayoutshowcase
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import hocchuc.com.constrainlayoutshowcase.R.id.btn1
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpView()
+
+    }
+
+    private fun setUpView() {
+        btn1.setOnClickListener({
+            startActivity<ExampleOneActivity>()
+        })
+        btn2.setOnClickListener({
+            startActivity<ExampleTwoActivity>()
+
+        })
+        btn3.setOnClickListener({
+            startActivity<ExampleThreeActivity>()
+        })
     }
 }
